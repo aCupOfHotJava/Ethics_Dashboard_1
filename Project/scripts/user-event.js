68f1634fbd1ba99ff0d41a8c79bbdc1279476b83
aCupOfHotJava <bkrieg1999@gmail.com>
2021-10-17 16:08:58 -0700

    tested out bulma css and jquery, set up django

---
+++ Project/scripts/user-event.js
@@ -0,0 +1,25 @@
+// Ethical solutions to a dilemma
+var numOptions = 0;
+$("#add-option").click(function() {
+    $("#ethics-options-wrapper").append('<textarea id = "option-text" class = "textarea" placeholder = "Enter your text here."></textarea>');
+    $("#empty").remove();
+    $("#add-option").hide();
+    $("#ethics-options-wrapper").append('<button class = "button" id = "submit-option">Submit</button>');
+    $("#submit-option").click(function() {
+        numOptions ++;
+        $("#submit-option").remove();
+        var text = $('textarea#option-text').val();
+        $('#ethics-options-wrapper').append('<p class = "header">Option ' + numOptions + '</p>');
+        $('#ethics-options-wrapper').append('<div class = "box">' + text + '</div>');
+        $('#option-text').remove();
+        $('#add-option').show();
+    })
+})
+
+// Ethical dilemma
+$('#add-dilemma').click(function() {
+    var text = $('#dilemma-text').val();
+    $('#dilemma-box').append('<p>' + text + '</p>');
+    $('#dilemma-text').remove();
+    $('#add-dilemma').remove();
+})
