4a9786bf9bae45eea7eb4102dfb810386b306226
Brandon Krieg <bkrieg1999@gmail.com>
2021-11-03 14:15:05 -0700

    added a bunch of stuff for login and account creation

--- Project/server/processLogin.php
+++ Project/server/processLogin.php
@@ -2,7 +2,20 @@
 <html>
     <?php
         try {
-            echo "test";
+
+            $connString = "mysql:host=localhost;dbname=ethics_db";
+            $user = "root";
+            $pass = "";
+            $pdo = new PDO($connString, $user, $pass);
+            $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql = "SELECT * FROM student";
+            $result = $pdo -> query($sql);
+
+            while($row = $result -> fetch()) {
+                echo $row['StudentID']. " - ". $row['UId']. " - "
+                        .$row['PId']. "<br/>";
+            }
         }
         catch(PDOException $e) {
             die($e -> getMessage());
