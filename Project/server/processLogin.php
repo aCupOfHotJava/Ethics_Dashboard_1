5e175ed0b5e7e0fab52fc5cb0178bdd24be57d95
Brandon Krieg <bkrieg1999@gmail.com>
2021-11-05 14:12:35 -0700

    login working + error checking, basic account creation functionality started, will finish creation and error checking tomorrow

--- Project/server/processLogin.php
+++ Project/server/processLogin.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
     <?php
+
+        // DO NOT handle login requests if request method is GET!!
+        if($_SERVER["REQUEST_METHOD"] == "GET") {
+            exit("Invalid request method!");
+        }
+
         try {
 
             $connString = "mysql:host=localhost;dbname=ethics_db";
@@ -9,12 +15,23 @@
             $pdo = new PDO($connString, $user, $pass);
             $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $sql = "SELECT * FROM student";
+            // Get form data in preparation for login query
+            $uid = $_POST["sid"];
+            $pid = $_POST["pid"];
+
+            // Query
+            $sql = "SELECT StudentID, PId FROM student WHERE StudentID = '". $uid.
+                    "' AND Pid = '". $pid. "'";
+            
             $result = $pdo -> query($sql);
 
+            $count = 0;
             while($row = $result -> fetch()) {
-                echo $row['StudentID']. " - ". $row['UId']. " - "
-                        .$row['PId']. "<br/>";
+                echo "Hello captain ". $row["PId"];
+                $count ++;
+            }
+            if($count == 0) {
+                echo "Invalid login information or account does not exist";
             }
         }
         catch(PDOException $e) {
