0e3b619b15328e611f8727dea58816962f531e81
Brandon Krieg <bkrieg1999@gmail.com>
2022-02-23 13:49:45 -0800

    Improved SQL and deontology main page, greatly improved instructor view page (still needs much more work)

--- Project/html/instructor-view-student.php
+++ Project/html/instructor-view-student.php
@@ -23,6 +23,7 @@
     catch(PDOException $e) {
         die($e -> getMessage());
     }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -30,33 +31,108 @@
         <title>Instructor Home Page</title>
         <link rel = "stylesheet" href = "../styles/bulma/css/bulma.css">
     </head>
-    <body>
-        <div class = "title is-6">Student Ethical Issues and Options</div>
+    <body class = "box has-background-dark">
+        <div class = "box has-background-primary-light">
+            <div class = "title is-6">Student Ethical Issues and Options</div>
             <header>Student Issue</header>
             <div class = "box has-background-lighter">
                 <?php
-                    $sql = "SELECT dilemma FROM ethical_issues WHERE uid = " .$uid;
-                    $result = $pdo -> query($sql);
-                    while($row = $result -> fetch()) {
-                        echo $row[0];
+                    try {
+                        $sql = "SELECT dilemma FROM ethical_issues WHERE uid = " .$uid;
+                        $result = $pdo -> query($sql);
+                        while($row = $result -> fetch()) {
+                            echo $row[0];
+                        }
+                    }
+                    catch(SQLException $e) {
+                        die($e -> getMessage());
                     }
                 ?>
             </div>
             <header>Student Options</header>
             <div class = "box has-background-lighter">
                 <?php
-                    $sql = "SELECT studentOption FROM ethical_options WHERE uid = " .$uid;
-                    $result = $pdo -> query($sql);
-                    while($row = $result -> fetch()) {
-                        echo "<div class = box>";
-                        echo $row[0];
-                        echo "</div>";
+                    try {
+                        $sql = "SELECT studentOption FROM ethical_options WHERE uid = " .$uid;
+                        $result = $pdo -> query($sql);
+                        while($row = $result -> fetch()) {
+                            echo "<div class = 'box'>";
+                            echo $row[0];
+                            echo "</div>";
+                        }
+                    }
+                    catch(SQLException $e) {
+                        die($e -> getMessage());
                     }
                 ?>
             </div>
         </div>
-        <div class = "title is-6">Student Stakeholder Analysis</div>
-            
+        <div class = "box has-background-primary-light">
+            <div class = "title is-6">Student Stakeholder Analysis</div>
+            <div class = "box has-background-lighter">
+                <?php
+                    echo "Stakeholder analysis not ready for SQL";
+                ?>
+            </div>
+        </div>
+        <div class = "box has-background-primary-light">
+            <div class = "title is-6">Student Utilitarianism</div>
+            <div class = "box has-background-lighter">
+                <?php
+                    try {
+                        $sql = "SELECT option1, option1Ex, option2, option2Ex FROM utilitarianism WHERE uid = " .$uid;
+                        $result = $pdo -> query($sql);
+                        while($row = $result -> fetch()) {
+                            echo "Student Option 1";
+                            echo "<div class = 'box'>";
+                            echo $row[0];
+                            echo "</div>";
+                            echo "Student Explanation 1";
+                            echo "<div class = 'box'>";
+                            echo $row[1];
+                            echo "</div>";
+                            echo "Student Option 2";
+                            echo "<div class = 'box'>";
+                            echo $row[2];
+                            echo "</div>";
+                            echo "Student Explanation 2";
+                            echo "<div class = 'box'>";
+                            echo $row[3];
+                            echo "</div>";
+                        }
+                    }
+                    catch(SQLException $e) {
+                        die($e -> getMessage());
+                    }
+                ?>
+            </div>
+        </div>
+        <div class = "box has-background-primary-light">
+            <div class = "title is-6">Student Deontology</div>
+            <div class = "box has-background-lighter">
+                <?php
+                    $sql = "SELECT option1, option2, checkbox1, checkbox2 FROM deontology WHERE uid = " .$uid;
+                    $result = $pdo -> query($sql);
+                    while($row = $result -> fetch()) {
+                        echo "Student Option 1";
+                        echo "<div class = 'box'>";
+                        echo $row[0];
+                        echo "</div>";
+                        echo "Student Reasons 1";
+                        echo "<div class = 'box'>";
+                        echo $row[2];
+                        echo "</div>";
+                        echo "Student Option 2";
+                        echo "<div class = 'box'>";
+                        echo $row[1];
+                        echo "</div>";
+                        echo "Student Reasons 2";
+                        echo "<div class = 'box'>";
+                        echo $row[3];
+                        echo "</div>";
+                    }
+                ?>
+            </div>
         </div>
     </body>
 </html>
