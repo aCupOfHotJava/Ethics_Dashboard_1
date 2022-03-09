3f1447569392bef4e1d355a66d6ca9534d335f8d
Alvin Villafranca <alphino22@gmail.com>
2022-03-09 12:16:46 -0800

    myProgress first implementation

--- Project/html/MyProgress.php
+++ Project/html/MyProgress.php
@@ -1,4 +1,7 @@
-
+<?php
+    session_start();
+    print_r($_SESSION);
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,51 +14,82 @@
     <body>
         
         <h1 class = "title has-background-grey-lighter">ETHICS DASHBOARD</h1>
-        <?php require_once(realpath($_SERVER["DOCUMENT_ROOT"]).'/Ethics_Dashboard_1/Project/server/MyProgress.php');
-                                    determineState();
-                             ?>
+        <?php
+    if($_SESSION == NULL) {
+        session_destroy();
+        header("Location: login.php");
+    }
+
+        $uid = $_SESSION["uid"];
+        $isEmpty = true;
+        try {
+            $connString = "mysql:host=lowe-walker.org;dbname=rwalker_Ethics_Dashboard_1";
+            $user = "rwalker_villafranca";
+            $pass = "gapsd5W2";
+            $pdo = new PDO($connString, $user, $pass);
+            $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            }
+        catch(PDOException $e) {
+            die($e -> getMessage());
+        }
+    
+    ?>
         <div class="columns">
         <div class="column is-two-fifths">
             <div class=" box has-background-grey-lighter">
                 <h3>ETHICAL ISSUE & OPTIONS</h3>
+
                 <h4>Comments Summary:</h4>
                 <?php
-                    try {
-                        $sql = "SELECT option1, option1Ex, option2, option2Ex FROM utilitarianism WHERE uid = " .$uid;
+                try {
+                        $sql = "SELECT ethicalIssuesandOptionsText FROM myProgress WHERE uid = " .$uid;
                         $result = $pdo -> query($sql);
-                        while($row = $result -> fetch()) {
-                            echo "Student Option 1";
-                            echo "<div class = 'box'>";
-                            echo $row[0];
-                            echo "</div>";
-                            echo "Student Explanation 1";
-                            echo "<div class = 'box'>";
-                            echo $row[1];
-                            echo "</div>";
-                            echo "Student Option 2";
-                            echo "<div class = 'box'>";
-                            echo $row[2];
-                            echo "</div>";
-                            echo "Student Explanation 2";
-                            echo "<div class = 'box'>";
-                            echo $row[3];
-                            echo "</div>";
-                        }
-                    }
-                    catch(SQLException $e) {
-                        die($e -> getMessage());
-                    }
+                         while($row = $result -> fetch()) {
+                         echo $row[0];
+
+                             }
+                                }
+                catch(SQLException $e) {
+                  die($e -> getMessage());
+                                    }
                 ?>
             </div>
 
             <div class=" box has-background-grey-lighter">
               <h3>Stakeholder </h3>
               <h4>Comments:</h4>
+              <?php
+                try {
+                        $sql = "SELECT stakeholderText FROM myProgress WHERE uid = " .$uid;
+                        $result = $pdo -> query($sql);
+                         while($row = $result -> fetch()) {
+                         echo $row[0];
+
+                             }
+                                }
+                catch(SQLException $e) {
+                  die($e -> getMessage());
+                                    }
+                ?>
             </div>
 
             <div class="virtueEthics box has-background-grey-lighter">
               <h3>VIRTUE ETHICS</h3>
               <h4>Comments:</h4>
+              <?php
+                try {
+                        $sql = "SELECT virtueEthicsText FROM myProgress WHERE uid = " .$uid;
+                        $result = $pdo -> query($sql);
+                         while($row = $result -> fetch()) {
+                         echo $row[0];
+
+                             }
+                                }
+                catch(SQLException $e) {
+                  die($e -> getMessage());
+                                    }
+                ?>
             </div>
 
  
@@ -66,16 +100,56 @@
             <div class="deontology box has-background-grey-lighter">
               <h3>DEONTOLOGY</h3>
               <h4>Comments Summary:</h4>
+              <?php
+                try {
+                        $sql = "SELECT deontologyText FROM myProgress WHERE uid = " .$uid;
+                        $result = $pdo -> query($sql);
+                         while($row = $result -> fetch()) {
+                         echo $row[0];
+
+                             }
+                                }
+                catch(SQLException $e) {
+                  die($e -> getMessage());
+                                    }
+                ?>
           </div>
 
           <div class="careEthics box has-background-grey-lighter">
               <h3>CARE ETHICS</h3>
               <h4>Comments:</h4>
+              <?php
+                try {
+                        $sql = "SELECT careEthicsText FROM myProgress WHERE uid = " .$uid;
+                        $result = $pdo -> query($sql);
+                         while($row = $result -> fetch()) {
+                         echo $row[0];
+
+                             }
+                                }
+                catch(SQLException $e) {
+                  die($e -> getMessage());
+                                    }
+                ?>
             </div>
 
             <div class="utilitarianism box has-background-grey-lighter">
                 <h3>UTILITARIANISM</h3>
                 <h4>Comments Summary:</h4>
+                <?php
+                try {
+                        $sql = "SELECT utilitarianismText FROM myProgress WHERE uid = " .$uid;
+                        $result = $pdo -> query($sql);
+                         while($row = $result -> fetch()) {
+                         echo $row[0];
+
+                             }
+                                }
+                catch(SQLException $e) {
+                  die($e -> getMessage());
+                                    }
+                ?>
+                
             </div>           
 
         </div>
@@ -104,18 +178,38 @@
                 </div>
                 <a class="box has-background-danger-light">
                 <h3>TOTAL</h3>
-                <p>
-                  Issue & Options: 6/10
-                  Stakeholders: 7.5/10
-                  Utilitarianism: 12/15
-                  Deontology: 0/15
-                  VirtueEthics: 0/15
-                  Care Ethics: 12.5/15
-                </p>
+                <?php
+                    try {
+                        $sql = "SELECT ethicalIssuesandOptionsScore, stakeholderScore, virtueEthicsScore, deontologyScore,careEthicsScore,utilitarianismScore FROM myProgress WHERE uid = " .$uid;
+                        $result = $pdo -> query($sql);
+                        while($row = $result -> fetch()) {
+                            echo "ethicalIssuesandOptionsScore: ";
+                            echo $row[0];
+                            echo "<br>";
+                            echo "stakeholderScore: ";
+                            echo $row[1];
+                            echo "<br>";
+                            echo "virtueEthicsScore: ";
+                            echo $row[2];
+                            echo "<br>";
+                            echo "deontologyScore: ";
+                            echo $row[3];
+                            echo "<br>";
+                            echo "careEthicsScore: ";
+                            echo $row[4];
+                            echo "<br>";
+                            echo "utilitarianismScore: ";
+                            echo $row[5];
+                        }
+                    }
+                    catch(SQLException $e) {
+                        die($e -> getMessage());
+                    }
+                ?>
               
                 </a>
             </div>
-            <a href="Project/html/MyProgress.php" download>
+            <a href="Project/html/MyProgress.txt" download>
             <h3>Download Report</h3>
           </a>
         </div>
